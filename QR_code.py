14916ec68025999adef09561b202bf19ac6b6e26
David Meira <74539212+dmeirap@users.noreply.github.com>
2023-01-05 16:56:26 +0100

    Add files via upload

---
+++ QR_code.py
@@ -0,0 +1,98 @@
+
+from PIL import Image, ImageColor,ImageDraw, ImageFont #pip install Pillow
+import cv2 #pip install opencv-python
+from pyzbar.pyzbar import decode #pip install pyzbar
+import numpy as np #pip install numpy
+
+cap = cv2.VideoCapture(0) #Capturar un video frame by frame
+cv2.namedWindow("Output", 0)
+while True:
+    ret, im = cap.read()
+    if not ret:
+        continue
+    size = im.shape #Tamaño de la imagen [0]x[1] = [width]x[height]
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY, dstCn=0)
+    
+    for d in decode(im):
+        print('decoded', d.type, 'symbol', '"%s"' % d.data) 
+        
+        topLeftCorners = (d.rect.left, d.rect.top)
+        topRightCorners = (d.rect.left + d.rect.width, d.rect.top) 
+        bottomLeftCorners = (d.rect.left, d.rect.top + d.rect.height)     
+        bottomRightCorners  = (d.rect.left + d.rect.width, d.rect.top + d.rect.height)  
+        
+        cv2.line(im, topLeftCorners, topRightCorners, (255, 0, 0), 2)
+        cv2.line(im, topLeftCorners, bottomLeftCorners, (255, 0, 0), 2)
+        cv2.line(im, topRightCorners, bottomRightCorners, (255, 0, 0), 2)
+        cv2.line(im, bottomLeftCorners, bottomRightCorners, (255, 0, 0), 2)
+        #im = cv2.putText(im, s, (d.rect.left, d.rect.top + d.rect.height+50),cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 0, 255), 2, cv2.LINE_AA)
+
+        image_points = np.array([
+            (int((topLeftCorners[0]+topRightCorners[0])/2),
+             int((topLeftCorners[1]+bottomLeftCorners[1])/2)),     # Nose
+            topLeftCorners,     # Left eye left corner
+            topRightCorners,     # Right eye right corne
+            bottomLeftCorners,     # Left Mouth cornerq
+            bottomRightCorners      # Right mouth corner
+        ], dtype="double")
+
+        # 3D model points.
+        model_points = np.array([
+            (0.0, 0.0, 0.0),             # Nose
+            # Left eye left corner
+            (-225.0, 170.0, -135.0),
+            # Right eye right corner
+            (225.0, 170.0, -135.0),
+            # Left Mouth corner
+            (-150.0, -150.0, -125.0),
+            # Right mouth corner
+            (150.0, -150.0, -125.0)
+
+        ],dtype="double")
+        
+        # Camera internals
+
+        focal_length = size[1]
+        center = (size[1]/2, size[0]/2)
+        camera_matrix = np.array(
+            [[focal_length, 0, center[0]],
+             [0, focal_length, center[1]],
+             [0, 0, 1]], dtype="double"
+        )
+
+        print("Camera Matrix :\n {0}".format(camera_matrix))
+        
+        dist_coeffs = np.zeros((4, 1),dtype="double")  # Assuming no lens distortion
+        
+        try:
+            (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_SQPNP)
+        except:
+            continue
+
+        print("Rotation Vector:\n {0}".format(rotation_vector))
+        print("Translation Vector:\n {0}".format(translation_vector))
+
+        # Project a 3D point (0, 0, 1000.0) onto the image plane. Define la distancia estimada a la que debe estar la camara del objeto
+        # We use this to draw a line sticking out of the nose
+
+        (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
+
+        for p in image_points:
+            cv2.circle(im, (int(p[0]), int(p[1])), 3, (0, 0, 255), -1)
+
+        p1 = (int(image_points[0][0]), int(image_points[0][1]))
+        p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+
+        # pillars
+        cv2.line(im, bottomLeftCorners, p2, (255, 0, 0), 2)
+        cv2.line(im, topLeftCorners, p2, (255, 0, 0), 2)
+        cv2.line(im, bottomRightCorners, p2, (255, 0, 0), 2)
+        cv2.line(im, topRightCorners, p2, (255, 0, 0), 2)
+
+        # Display image
+    cv2.imshow("Output", im)
+
+    # Wait for the magic key
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.destroyWindow("Output")
