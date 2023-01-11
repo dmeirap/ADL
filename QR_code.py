6cdebc60ca36b16aa2064d5f9ff83a07c9c14812
David Meira <74539212+dmeirap@users.noreply.github.com>
2023-01-11 16:32:50 +0100

    Add files via upload

--- QR_code.py
+++ QR_code.py
@@ -16,7 +16,8 @@ while True:
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY, dstCn=0)
     
     for d in decode(im):
-        print('decoded', d.type, 'symbol', '"%s"' % d.data) 
+        
+        #print('decoded', d.type, 'symbol', '"%s"' % d.data) 
         
         topLeftCorners = (d.rect.left, d.rect.top)
         topRightCorners = (d.rect.left + d.rect.width, d.rect.top) 
@@ -48,37 +49,44 @@ while True:
             (-46.50, -46.50, 0),
             # Right bottom corner
             (46.50, -46.50, 0)
-
         ],dtype="double")
         
         #************ SET PARAMETROS DE CAMARA *********************
         
         camera_matrix = np.array(
-            [[781.45492812, 0, 932.52867315],
-             [0,  782.46840213, 575.57313795],
+            [[773.74013464, 0, 957.40574841],
+             [0,  774.15094158, 560.68397322],
              [0, 0, 1]], dtype="double"
         )
         
         dist_coeffs = np.zeros((5, 1),dtype="double")  # Lens distortion
-        dist_coeffs[0,0] = 0.006589358509533379
-        dist_coeffs[1,0] = -0.060555647700596946
-        dist_coeffs[2,0] = -0.004607629905733067
-        dist_coeffs[3,0] = -0.0035826292149932857
-        dist_coeffs[4,0] = 0.038915448056452626
+        dist_coeffs[0,0] = -0.005905527127455908
+        dist_coeffs[1,0] = 0.03188619089891427
+        dist_coeffs[2,0] = -0.008733485736477005
+        dist_coeffs[3,0] = -0.0007170139797843391
+        dist_coeffs[4,0] = -0.06968794979128276
     
         try:
             (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_SQPNP)
         except:
             continue
 
-        print("Rotation Vector:\n {0}".format(rotation_vector))
-        print("Translation Vector:\n {0}".format(translation_vector))
+        rmat, jac = cv2.Rodrigues(rotation_vector)
+        angles, mtxR, mtxQ, Qx, Qy, Qz = cv2.RQDecomp3x3(rmat)
+        
+        print("\nRotation X: {0}".format(angles[0]))
+        print("Rotation Y: {0}".format(angles[1]))
+        print("Rotation Z: {0}".format(angles[2]))
+        print("\nTranslation X: {0}".format(translation_vector[0]))
+        print("Translation Y: {0}".format(translation_vector[1]))
+        print("Translation Z: \n {0}".format(translation_vector[2]))
 
         # Project a 3D point (0, 0, 1000.0) onto the image plane. Define la distancia estimada a la que debe estar la camara del objeto
         (center_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1500.0)]), rotation_vector, translation_vector, camera_matrix, dist_coeffs)
 
         for p in image_points:
             cv2.circle(im, (int(p[0]), int(p[1])), 3, (0, 0, 255), -1)
+            
 
         p1 = (int(image_points[0][0]), int(image_points[0][1]))
         p2 = (int(center_end_point2D[0][0][0]), int(center_end_point2D[0][0][1]))
@@ -87,8 +95,8 @@ while True:
         cv2.line(im, bottomLeftCorners, p2, (255, 0, 0), 2)
         cv2.line(im, topLeftCorners, p2, (255, 0, 0), 2)
         cv2.line(im, bottomRightCorners, p2, (255, 0, 0), 2)
-        cv2.line(im, topRightCorners, p2, (255, 0, 0), 2)
-        
+        cv2.line(im, topRightCorners, p2, (255, 0, 0), 2)     
+                
     cv2.imshow("Output", im)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
