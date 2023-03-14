1bdd8f1c74fd4ee9cb3e0e184b69251d1250c6cc
David Meira <74539212+dmeirap@users.noreply.github.com>
2023-03-14 16:56:05 +0100

    Add files via upload

---
+++ build/CMakeFiles/qr_src.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/qr_src.dir/qr_code.cpp.o"
+  "qr_src"
+  "qr_src.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/qr_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
